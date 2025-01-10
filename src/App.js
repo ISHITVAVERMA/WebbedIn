5e26454b00065b23abf809be917882ee18065d6e
Ishitva Verma <i_shit@Ishitvas-MacBook-Air.local>
2025-01-10 16:55:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Provider } from "../src/components/ui/provider";
+import { Tabs, Flex } from "@chakra-ui/react";
+import { LuFolder, LuSquareCheck, LuUser } from "react-icons/lu";
+
+function App() {
+  return (
+    <Provider>
+      
+    </Provider>
+  );
+}
+
+export default App;
